b2a8ed224444d7b7f294318ce919860f4d5c5465
Muhiddin <88494561+Muhiddin0@users.noreply.github.com>
2025-01-09 15:05:18 +0500

    `baseURL ni muhit o'zgaruvchidan olish`

--- services/instace.ts
+++ services/instace.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:8000/api/v1/"
-      : "http://127.0.0.1:8000/api/v1/",
+  baseURL: process.env.BASE_URL,
 });
 
 export default instance;
