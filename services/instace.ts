15ba67b4718c802536c3c80f61ece9d29fc2cb4f
Muhiddin <88494561+Muhiddin0@users.noreply.github.com>
2025-01-09 15:13:29 +0500

    Base URL ni rivojlanish va ishlab chiqarish muhitiga qarab o'zgartirildigit push

--- services/instace.ts
+++ services/instace.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://127.0.0.1:8000/api/v1/"
+      : "https://rakat-solutions-website-backend.6cpoqp.easypanel.host/api/v1/",
 });
 
 export default instance;
